9555221ec124b7850fb096cc38a01ed520f2d96e
brecht.vermeersch <brecht.vermeersch@athena-graphics.be>
2024-07-06 16:54:48 +0200

    add retry logic

--- src/Common/Middleware/ClientFactory.php
+++ src/Common/Middleware/ClientFactory.php
@@ -8,6 +8,7 @@ use AzureOss\Storage\Common\ApiVersion;
 use AzureOss\Storage\Common\Auth\StorageSharedKeyCredential;
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
+use GuzzleRetry\GuzzleRetryMiddleware;
 
 /**
  * @internal
@@ -18,6 +19,7 @@ final class ClientFactory
     {
         $handlerStack = HandlerStack::create();
 
+        $handlerStack->push(new AddXMsClientRequestIdMiddleware());
         $handlerStack->push(new AddXMsDateHeaderMiddleware());
         $handlerStack->push(new AddXMsVersionMiddleware(ApiVersion::LATEST));
 
@@ -25,6 +27,25 @@ final class ClientFactory
             $handlerStack->push(new AddAuthorizationHeaderMiddleware($sharedKeyCredential));
         }
 
+        $handlerStack->push($this->createRetryMiddleware());
+
         return new Client(['handler' => $handlerStack]);
+    }
+
+    /**
+     * @see https://learn.microsoft.com/en-us/azure/architecture/best-practices/retry-service-specific#general-rest-and-retry-guidelines
+     */
+    private function createRetryMiddleware(): \Closure
+    {
+        return GuzzleRetryMiddleware::factory([
+            'retry_on_status' => [
+                408, // Request Timeout
+                429, // Too Many Requests
+                500, // Internal Server Error
+                502, // Bad Gateway
+                503, // Service Unavailable
+                504, // Gateway Timeout
+            ],
+        ]);
     }
 }
