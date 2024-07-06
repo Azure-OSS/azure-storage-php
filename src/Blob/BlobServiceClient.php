68a200f10bcc23f439833218e9cc36d811b213d1
Brecht <brechtvermeersch@outlook.be>
2024-07-06 22:37:01 +0200

    improve connection string parsing (#10)

--- src/Blob/BlobServiceClient.php
+++ src/Blob/BlobServiceClient.php
@@ -4,20 +4,13 @@ declare(strict_types=1);
 
 namespace AzureOss\Storage\Blob;
 
+use AzureOss\Storage\Blob\Exceptions\InvalidConnectionStringException;
 use AzureOss\Storage\Common\Auth\StorageSharedKeyCredential;
-use GuzzleHttp\Psr7\Uri;
+use AzureOss\Storage\Common\ConnectionStringParser;
 use Psr\Http\Message\UriInterface;
 
 final class BlobServiceClient
 {
-    private const DEV_CONNECTION_STRING_SHORTCUT = 'UseDevelopmentStorage=true';
-
-    private const DEV_BLOB_ENDPOINT = 'http://127.0.0.1:10000/devstoreaccount1';
-
-    private const DEV_BLOB_ACCOUNT_NAME = "devstoreaccount1";
-
-    private const DEV_BLOB_ACCOUNT_KEY = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==";
-
     public function __construct(
         public UriInterface $uri,
         public readonly ?StorageSharedKeyCredential $sharedKeyCredentials = null,
@@ -25,41 +18,23 @@ final class BlobServiceClient
 
     public static function fromConnectionString(string $connectionString): self
     {
-        if ($connectionString === self::DEV_CONNECTION_STRING_SHORTCUT) {
-            return self::fromDevConnectionString();
+        $uri = ConnectionStringParser::getBlobEndpoint($connectionString);
+        if ($uri === null) {
+            throw new InvalidConnectionStringException();
         }
 
-        $settings = [];
-        foreach (explode(';', $connectionString) as $segment) {
-            if (!empty($segment)) {
-                [$key, $value] = explode('=', $segment, 2);
-                $settings[$key] = $value;
-            }
+        $sas = ConnectionStringParser::getSas($connectionString);
+        if($sas !== null) {
+            return new self($uri->withQuery($sas));
         }
 
-        $blobEndpoint = $settings['BlobEndpoint']
-            ?? sprintf(
-                '%s://%s.blob.%s',
-                $settings['DefaultEndpointsProtocol'],
-                $settings['AccountName'],
-                $settings['EndpointSuffix'],
-            );
+        $accountName = ConnectionStringParser::getAccountName($connectionString);
+        $accountKey = ConnectionStringParser::getAccountKey($connectionString);
+        if ($accountName !== null && $accountKey !== null) {
+            return new self($uri, new StorageSharedKeyCredential($accountName, $accountKey));
+        }
 
-        return new self(
-            new Uri($blobEndpoint),
-            new StorageSharedKeyCredential($settings['AccountName'], $settings['AccountKey']),
-        );
-    }
-
-    private static function fromDevConnectionString(): self
-    {
-        return new self(
-            new Uri(self::DEV_BLOB_ENDPOINT),
-            new StorageSharedKeyCredential(
-                self::DEV_BLOB_ACCOUNT_NAME,
-                self::DEV_BLOB_ACCOUNT_KEY,
-            ),
-        );
+        throw new InvalidConnectionStringException();
     }
 
     public function getContainerClient(string $containerName): BlobContainerClient
--- tests/Blob/Feature/BlobServiceClientTest.php
+++ tests/Blob/Feature/BlobServiceClientTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace AzureOss\Storage\Tests\Blob\Feature;
 
 use AzureOss\Storage\Blob\BlobServiceClient;
+use AzureOss\Storage\Blob\Exceptions\InvalidConnectionStringException;
 use AzureOss\Storage\Tests\Blob\BlobFeatureTestCase;
 use PHPUnit\Framework\Attributes\Test;
 
@@ -44,6 +45,57 @@ final class BlobServiceClientTest extends BlobFeatureTestCase
         $this->assertEquals('devstoreaccount1', $client->sharedKeyCredentials->accountName);
         $this->assertEquals('Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==', $client->sharedKeyCredentials->accountKey);
         $this->assertEquals("http://127.0.0.1:10000/devstoreaccount1", (string) $client->uri);
+    }
+
+    #[Test]
+    public function from_connection_string_without_account_name_throws(): void
+    {
+        $this->expectException(InvalidConnectionStringException::class);
+        $connectionString = "DefaultEndpointsProtocol=https;AccountKey=Y2hlZXNlMWNoZWVzZTEyY2hlZXNlMTIzCg==;EndpointSuffix=core.windows.net";
+        BlobServiceClient::fromConnectionString($connectionString);
+    }
+
+    #[Test]
+    public function from_connection_string_without_account_key_throws(): void
+    {
+        $this->expectException(InvalidConnectionStringException::class);
+        $connectionString = "DefaultEndpointsProtocol=https;AccountName=testing;EndpointSuffix=core.windows.net";
+        BlobServiceClient::fromConnectionString($connectionString);
+    }
+
+    #[Test]
+    public function from_connection_string_without_blob_endpoint_and_without_endpoint_suffix_throws(): void
+    {
+        $this->expectException(InvalidConnectionStringException::class);
+        $connectionString = "DefaultEndpointsProtocol=https;AccountName=testing;AccountKey=Y2hlZXNlMWNoZWVzZTEyY2hlZXNlMTIzCg==";
+        BlobServiceClient::fromConnectionString($connectionString);
+    }
+
+    #[Test]
+    public function from_connection_string_with_sas_works(): void
+    {
+        $connectionString = "BlobEndpoint=https://storagesample.blob.core.windows.net;SharedAccessSignature=sv=2015-07-08&sig=iCvQmdZngZNW%2F4vw43j6%2BVz6fndHF5LI639QJba4r8o%3D&spr=https&st=2016-04-12T03%3A24%3A31Z&se=2016-04-13T03%3A29%3A31Z&srt=s&ss=bf&sp=rwl";
+        $client = BlobServiceClient::fromConnectionString($connectionString);
+
+        $this->assertNull($client->sharedKeyCredentials);
+        $this->assertEquals("https://storagesample.blob.core.windows.net?sv=2015-07-08&sig=iCvQmdZngZNW%2F4vw43j6%2BVz6fndHF5LI639QJba4r8o%3D&spr=https&st=2016-04-12T03%3A24%3A31Z&se=2016-04-13T03%3A29%3A31Z&srt=s&ss=bf&sp=rwl", (string) $client->uri);
+    }
+
+    #[Test]
+    public function from_connection_string_without_account_key_and_without_sas_throws(): void
+    {
+        $this->expectException(InvalidConnectionStringException::class);
+        $connectionString = "BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;";
+        BlobServiceClient::fromConnectionString($connectionString);
+    }
+
+    #[Test]
+    public function from_connection_string_default_endpoint_protocol_overwrites_protocol_of_blob_endpoint(): void
+    {
+        $connectionString = "DefaultEndpointsProtocol=https;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;";
+        $client = BlobServiceClient::fromConnectionString($connectionString);
+
+        $this->assertEquals("https://127.0.0.1:10000/devstoreaccount1", (string) $client->uri);
     }
 
     #[Test]
