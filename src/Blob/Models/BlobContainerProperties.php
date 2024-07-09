e9c7c129216f820dec7633079bb0e83b93749d18
brecht.vermeersch <brecht.vermeersch@athena-graphics.be>
2024-07-09 17:40:57 +0200

    add listing containers

--- src/Blob/Models/BlobContainerProperties.php
+++ src/Blob/Models/BlobContainerProperties.php
@@ -4,4 +4,26 @@ declare(strict_types=1);
 
 namespace AzureOss\Storage\Blob\Models;
 
-final class BlobContainerProperties {}
+use AzureOss\Storage\Blob\Exceptions\DateMalformedStringException;
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\Type;
+use Psr\Http\Message\ResponseInterface;
+
+final class BlobContainerProperties
+{
+    public function __construct(
+        #[SerializedName('Last-Modified')]
+        #[Type("DateTimeImmutable<'" . \DateTimeInterface::RFC1123 . "'>")]
+        public readonly \DateTimeInterface $lastModified,
+    ) {}
+
+    public static function fromResponseHeaders(ResponseInterface $response): self
+    {
+        $lastModified = \DateTimeImmutable::createFromFormat(\DateTimeInterface::RFC1123, $response->getHeaderLine('Last-Modified'));
+        if ($lastModified === false) {
+            throw new DateMalformedStringException("Azure returned a malformed date.");
+        }
+
+        return new self($lastModified);
+    }
+}
