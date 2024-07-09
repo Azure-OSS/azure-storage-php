36aa27d781b3c8a14d86cdd7468c4fd9a76ed579
Brecht <brechtvermeersch@outlook.be>
2024-07-09 21:50:59 +0200

    add listing containers (#18)
    * add listing containers
    
    * make classes final
    
    * add setting and getting container metadata (#16)
    
    Co-authored-by: brecht.vermeersch <brecht.vermeersch@athena-graphics.be>
    
    * add metadata on blobs (#20)
    
    * add metadata on blobs
    
    * make classes final
    
    ---------
    
    Co-authored-by: brecht.vermeersch <brecht.vermeersch@athena-graphics.be>
    
    * add listing containers
    
    * make classes final
    
    * fix merge
    
    ---------
    
    Co-authored-by: brecht.vermeersch <brecht.vermeersch@athena-graphics.be>

---
+++ src/Blob/Responses/ListContainersResponseBody.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AzureOss\Storage\Blob\Responses;
+
+use AzureOss\Storage\Blob\Models\BlobContainer;
+use JMS\Serializer\Annotation\XmlList;
+
+/**
+ * @internal
+ */
+final class ListContainersResponseBody
+{
+    /**
+     * @param BlobContainer[]  $containers
+     */
+    public function __construct(
+        public readonly string $prefix,
+        public readonly string $marker,
+        public readonly int $maxResults,
+        public readonly string $nextMarker,
+        #[XmlList(entry: 'Container')]
+        public readonly array $containers,
+    ) {}
+}
