1962b36c5f65b0032eb8fd69aed6754506ff4773
pierrebateau <peter@yournextconcepts.com>
2025-11-17 12:32:51 +0100

    Add option to disable SSL verification for local Azurite support with self-signed certificates (#78)
    * Add option to disable SSL verification for local Azurite support with self-signed certificates
    
    * Update docblock

--- src/Common/Middleware/HttpClientOptions.php
+++ src/Common/Middleware/HttpClientOptions.php
@@ -9,16 +9,18 @@ final class HttpClientOptions
     public function __construct(
         public readonly ?int $timeout = null,
         public readonly ?int $connectTimeout = null,
+        public readonly ?bool $verifySsl = null,
     ) {}
 
     /**
-     * @return array{timeout?: int, connect_timeout?: int}
+     * @return array{timeout?: int, connect_timeout?: int, verify?: bool}
      */
     public function toGuzzleHttpClientConfig(): array
     {
         return array_filter([
             'timeout' => $this->timeout,
             'connect_timeout' => $this->connectTimeout,
+            'verify' => $this->verifySsl,
         ], fn($value) => $value !== null);
     }
 }
